890b16429b3f19bfd4318329b066fed5156b68ca
Ting-YA-Yang <30425266+Ting-YA-Yang@users.noreply.github.com>
2024-06-15 06:40:20 +0000

    Deploying to gh-pages from @ Ting-YA-Yang/my-spec@8223f4add1279ea1a2a387d83259c92a75eeede0 🚀

--- docs/README.md
+++
@@ -1,6 +0,0 @@
-
-# Head  
-
-## Item  
-
-### Detail
--- packages/commitlint-config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // parserPreset: 'conventional-changelog-conventionalcommits',
-    rules: {
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert']],
-    },
-  };
